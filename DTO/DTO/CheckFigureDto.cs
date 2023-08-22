968c4905bee2b4037cc486618e740f771a9bbbd8
AlexanderYanakov <yanakov_222@mail.ru>
2023-08-22 12:47:52 +0300

    Update CheckFigureDto.cs

--- DTO/DTO/CheckFigureDto.cs
+++ DTO/DTO/CheckFigureDto.cs
@@ -10,12 +10,12 @@ namespace DTO.DTO
     public class CheckFigureDto
     {
         public string Name { get; set; }
-        public string Centroid { get; set; }
+        public string LatLng { get; set; }
 
-        public CheckFigureDto(string name, string centroid)
+        public CheckFigureDto(string name, string latLng)
         {
             Name = name;
-            Centroid = centroid;
+            LatLng = latLng;
         }
     }
 }
